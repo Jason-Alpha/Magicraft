66d83ad48b0f6fcd8bc8e51433bfbdc416032ca4
JasonAlpha-MODDER <64051651+JasonAlpha-MODDER@users.noreply.github.com>
2020-08-24 09:20:19 +0800

    Create CommonProxy.java

---
+++ src/main/java/com/github/JasonAlpha/Magicraft/common/CommonProxy.java
@@ -0,0 +1,37 @@
+/*
+ * Alpha Code制作
+ *
+ * 魔法禁书目录 Magicraft 0.0.1 Pre-alpha gametest
+ *
+ *
+ */
+
+
+
+
+
+
+package com.github.JasonAlpha.Magicraft.common;
+
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class CommonProxy
+{
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
